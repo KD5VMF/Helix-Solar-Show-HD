9f123484f4c776674f759fcf0a1ac768d133a545
KD5VMF <54245381+KD5VMF@users.noreply.github.com>
2026-04-05 20:41:39 -0500

    Delete PUBLISH_WIN64.ps1

--- PUBLISH_WIN64.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Set-Location $PSScriptRoot
-$project = Join-Path $PSScriptRoot 'src\HelixSolarShow\HelixSolarShow.csproj'
-dotnet publish $project -c Release -r win-x64 --self-contained false /p:PublishSingleFile=false
